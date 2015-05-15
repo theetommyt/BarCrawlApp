7f0e01738d596502af97f4ad877246e6e8df2bad
Tom Taylor <tom.taylor@outlook.com>
2015-05-15 12:16:30 -0500

    changed styling to brighter theme

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515152358) do
+ActiveRecord::Schema.define(version: 20150515162054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20150515152358) do
     t.float    "lat"
     t.float    "long"
     t.float    "loc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.integer  "drinker_id"
+    t.string   "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
