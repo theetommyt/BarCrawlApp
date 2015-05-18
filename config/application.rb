d403fed4da0e3fed13f09b0bbee5ffaa3787435f
Matt Lissner <mlissne@gmail.com>
2015-05-18 13:57:45 -0500

    rails gen

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ end
 
 module BarCrawl
   class Application < Rails::Application
+    require 'rails/generators'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
